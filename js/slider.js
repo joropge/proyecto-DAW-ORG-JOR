9569845098d6b0648c73792d7800df214c86a118
Alex <aleexsantos1509@gmail.com>
2024-02-09 12:32:04 +0100

    Converted all index styles to tailwind.

--- js/slider.js
+++ js/slider.js
@@ -1,21 +1,23 @@
 export function slider() {
     const images = document.querySelectorAll(".slider-img");
     let currentIndex = 0;
-  
+
     function showImage(index) {
-      images.forEach((image, i) => {
-        if (i === index) {
-          image.classList.add("active");
-        } else {
-          image.classList.remove("active");
-        }
-      });
+        images.forEach((image, i) => {
+            if (i === index) {
+                image.classList.remove("opacity-0");
+                image.classList.add("opacity-100");
+            } else {
+                image.classList.remove("opacity-100");
+                image.classList.add("opacity-0");
+            }
+        });
     }
-  
+
     function nextImage() {
-      currentIndex = (currentIndex + 1) % images.length;
-      showImage(currentIndex);
+        currentIndex = (currentIndex + 1) % images.length;
+        showImage(currentIndex);
     }
-  
+
     setInterval(nextImage, 5000);
 }
