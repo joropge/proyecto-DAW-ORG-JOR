6d1eca8ddec090375a306c70d4efbf7b380847b0
Mario <maalnu93@hotmail.com>
2024-03-07 09:57:02 +0100

    estilos finales admin

--- admin/index.php
+++ admin/index.php
@@ -97,52 +97,85 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['editarProducto'])) {
 <body class="bg-black text-white font-sans">
 <main class="container mx-0 px-4 py-8">
     <h1 class="text-yellow-500 text-3xl font-bold mb-4">Administrador de productos Ezequiel</h1>
-    <div class="flex justify-between items-center h-16">   
-                       
-        <input type="text" id="search" class="w-80 border-2 border-gray-800 px-5 pr-16 rounded-lg text-sm focus:outline-none h-full text-black" placeholder="Search">
+    <div>
+        <h2 class="text-white text-2xl font-bold mb-4">Productos</h2>
+        <div class="flex justify-between items-center" style="height: 3rem;">
+        
+            <input type="text" id="search" class="w-80 border-2 border-gray-800 px-5 pr-16 rounded-lg text-sm focus:outline-none h-full text-black" style="height:100%; width:40rem;" placeholder="Buscar por nombre...">
+        
+            <a href="./actions/crear-producto.php"
+                    class="inline-block bg-green-500 text-white px-4 py-2 rounded-md hover:bg-green-600 transition duration-300 ease-in-out" style="height: 100%;">Añadir
+                    producto</a>
+        </div>
+        
+            <table class="mt-8 w-full">
+        <thead class="border-b-2 border-green-500">
+        <tr class="border-b-2 border-green-500">
+        <th class="px-4 py-2 text-xl">Nombre</th>
+        <th class="px-4 py-2 text-xl">Racion</th>
+        <th class="px-4 py-2 text-xl">Precio KG</th>
+        <th class="px-4 py-2 text-xl">Imagen</th>
+        <th class="px-4 py-2 text-xl">Acción</th>
+        </tr>
+        </thead>
+        <tbody class="odd:bg-slate-300">
+        <?php
+                    // Your PHP code to display the products from the "productos" table goes here
+                    $query = "SELECT * FROM productos";
+                    $resultado = mysqli_query($db, $query);
                     
-        <a href="./actions/crear-producto.php"
-                class="inline-block bg-green-500 text-white px-4 py-2 rounded-md hover:bg-green-600 transition duration-300 ease-in-out">Añadir
-                producto</a>
+        while ($row = mysqli_fetch_assoc($resultado)) {
+        echo "<tr class='h-full'>";
+        echo "<td id='nameSearch' class='px-4 py-2 text-center'>" . $row['nombre'] . "</td>";
+        echo "<td class='px-4 py-2 text-center'>" . $row['racion'] . "</td>";
+        echo "<td class='px-4 py-2 text-center'>" . $row['precioKg'] . "</td>";
+        if (!empty($row['imagen'])) {
+            echo "<td class=' h-[150px] w-[150px] overflow-auto p-3'><img class='h-[150px] w-[150px] rounded-lg' src='../imagenes/" . $row['imagen'] . "  '></td>";
+        } else {
+            echo "<td class='h-[150px] w-[150px] p-3'><div class='rounded-lg h-[150px] w-[150px] grid place-content-center ' style='background-color: #404040; color: gray;'>" . $row['nombre'] . "</div></td>";
+        }
+        echo "<td class='px-4 py-2 mx-5'>
+        <div class=' w-full h-full flex flex-col justify-center items-center gap-2'>
+            <a href=" . htmlspecialchars($_SERVER["PHP_SELF"]) . "?borrarProducto=" . $row["id"] . " id='delete-btn' class='inline-block bg-red-500 text-white px-8 py-2 rounded-md hover:bg-red-600 transition duration-300 ease-in-out'>Borrar</a>
+            </br>
+            <a href='./actions/editar-producto.php?id=" . $row["id"] . "' id='edit-btn' class='inline-block bg-blue-500 text-white px-8 py-2 rounded-md hover:bg-blue-600 transition duration-300 ease-in-out'>Editar</a>
+        </div>
+            </td>";
+        //Añadir enlace para editar
+        
+        echo "</tr>";
+        }
+        ?>
+                </tbody>
+        </table>
     </div>
-    
+
+    <br><br><br>
+
+<h2 class="text-white text-2xl font-bold mb-4">Pedidos</h2>
+        
  
-        <table class="mt-8 w-full">
+<table class="mt-8 w-full">
 <thead class="border-b-2 border-green-500">
 <tr class="border-b-2 border-green-500">
-<th class="px-4 py-2 text-xl">Nombre</th>
-<th class="px-4 py-2 text-xl">Racion</th>
-<th class="px-4 py-2 text-xl">Precio KG</th>
-<th class="px-4 py-2 text-xl">Imagen</th>
-<th class="px-4 py-2 text-xl">Acción</th>
+<th class="px-4 py-2 text-xl">Usuario</th>
+<th class="px-4 py-2 text-xl">Fecha Pedido</th>
+<th class="px-4 py-2 text-xl">Importe Total</th>
+
 </tr>
 </thead>
 <tbody class="odd:bg-slate-300">
 <?php
-                // Your PHP code to display the products from the "productos" table goes here
-                $query = "SELECT * FROM productos";
-                $resultado = mysqli_query($db, $query);
- 
-                
+// Your PHP code to display the products from the "pedidos" table goes here
+$query = "SELECT pedidos.*, usuarios.nombre FROM pedidos INNER JOIN usuarios ON pedidos.idUser = usuarios.id";
+$resultado = mysqli_query($db, $query);
+
 while ($row = mysqli_fetch_assoc($resultado)) {
     echo "<tr class='h-full'>";
     echo "<td id='nameSearch' class='px-4 py-2 text-center'>" . $row['nombre'] . "</td>";
-    echo "<td class='px-4 py-2 text-center'>" . $row['racion'] . "</td>";
-    echo "<td class='px-4 py-2 text-center'>" . $row['precioKg'] . "</td>";
-    if (!empty($row['imagen'])) {
-        echo "<td class=' h-[150px] w-[150px] overflow-auto'><img class='h-[150px] w-[150px]' src='../imagenes/" . $row['imagen'] . "  '></td>";
-    } else {
-        echo "<td class='h-[150px] w-[150px] '><img src='https://via.placeholder.com/150'></td>";
-    }
-    echo "<td class='px-4 py-2 mx-5'>
-    <div class=' w-full h-full flex flex-col justify-center items-center gap-2'>
-        <a href=" . htmlspecialchars($_SERVER["PHP_SELF"]) . "?borrarProducto=" . $row["id"] . " id='delete-btn' class='inline-block bg-red-500 text-white px-8 py-2 rounded-md hover:bg-red-600 transition duration-300 ease-in-out'>Borrar</a>
-        </br>
-        <a href='./actions/editar-producto.php?id=" . $row["id"] . "' id='edit-btn' class='inline-block bg-blue-500 text-white px-8 py-2 rounded-md hover:bg-blue-600 transition duration-300 ease-in-out'>Editar</a>
-    </div>    
-        </td>";
-    //Añadir enlace para editar
-    
+    echo "<td class='px-4 py-2 text-center'>" . $row['fechaPedido'] . "</td>";
+    echo "<td class='px-4 py-2 text-center'>" . $row['precioTotal'] . "</td>";
+
     echo "</tr>";
 }
 ?>
